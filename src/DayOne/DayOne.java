313f19eed8693eb1032edb8ea8ee9f7e11878649
Marko <markoanastasijeski01@gmail.com>
2021-05-04 17:22:26 +0200

    first commit

---
+++ src/DayOne/DayOne.java
@@ -0,0 +1,15 @@
+package DayOne;
+
+public class DayOne {
+
+	public static void main(String[] args) {
+	System.out.println("Hello Java");
+	System.out.println("Hello World");
+	String name = "Marko";
+	System.out.println("Hi from:" + name);
+	System.out.println(args[0]);
+	System.out.println(args[1]);
+	System.out.println(args[2]);
+	}
+
+}
